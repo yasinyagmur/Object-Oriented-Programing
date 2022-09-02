78398d25705e7e2a6eb06b7c72bcae78aea9dace
yasin yağmur <yasin.yagmur@gmail.com>
2022-09-02 09:58:21 +0300

    ch1 lesson1

---
+++ Chapter_01/defination_start.py
@@ -0,0 +1,12 @@
+# Todo: create a basic class
+class Book:
+    def __init__(self,title):
+        self.title= title
+        
+# Todo: create instance of the class
+b1 = Book("Brave New World")
+b2= Book("War and Peace")
+
+# Todo: print the class and property
+print(b1)
+print(b1.title)
