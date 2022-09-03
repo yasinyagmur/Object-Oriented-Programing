9515c4c98c7fd29ccbdd89478c1d212ff0aa2f13
yasin yağmur <yasin.yagmur@gmail.com>
2022-09-03 23:01:08 +0300

    ch1 instance lesson coun.

--- Chapter_01/instance_start.py
+++ Chapter_01/instance_start.py
@@ -11,19 +11,29 @@ class Book:
         self.author= author
         self.pages= pages
         self.price= price
+        self.__secret ="This is a secret attribute"
 
 
     # TODO: create instance methods
+    def getprice(self):
+        if hasattr(self,"_discount"):
+            return self.price-(self.price*self._discount)
+        return self.price
+    def setdiscount(self,amount):
+        self._discount=amount
 
 
 # TODO: create some book instances
-b1 = Book("War and Peace")
-b2 = Book("The Catcher in the Rye")
+b1 = Book("War and Peace","Jason Rain",1989,33,50)
+b2 = Book("The Catcher in the Rye","Yasin Yağmur",1962,38.95, )
 
 # TODO: print the price of book1
-
+# print(b1.getprice())
 
 # TODO: try setting the discount
-
+# print(b2.getprice())
+# b2.setdiscount(0.25)
+# print(b2.getprice())
 
 # TODO: properties with double underscores are hidden by the interpreter
+print(b2.__secret)
